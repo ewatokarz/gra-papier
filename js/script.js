071643ad9e96f74723748a52a224cee31d7d251a
Ewa Tokarz <ewa.tokarz@yahoo.com>
2022-09-01 16:01:20 +0200

    add addEventListener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove;
 
-console.log("Wylosowana liczba to: " + randomNumber);
+playGame();
 
-let computerMove = getMoveName(randomNumber);
+function playGame() {
+  computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+  printMessage("Ruch komputera to: " + computerMove);
+
+  document.getElementById("kamien").addEventListener("click", function () {
+    buttonClicked(1);
+  });
+  document.getElementById("papier").addEventListener("click", function () {
+    buttonClicked(2);
+  });
+  document.getElementById("nozyce").addEventListener("click", function () {
+    buttonClicked(3);
+  });
+}
+
+function buttonClicked(argMoveId) {
+  let playerMove;
+  if (argMoveId == 1) {
+    playerMove = "kamien";
+  } else if (argMoveId == 2) {
+    playerMove = "nozyce";
+  } else if (argMoveId == 3) {
+    playerMove = "papier";
+  } else {
+    printMessage("Nie znam ruchu o id " + argMoveId + ".");
+    return;
+  }
+  displayResult(computerMove, playerMove);
+}
 
 function getMoveName(argMoveId) {
   if (argMoveId == 1) {
@@ -12,28 +40,9 @@ function getMoveName(argMoveId) {
   } else if (argMoveId == 3) {
     return "papier";
   }
-
   printMessage("Nie znam ruchu o id " + argMoveId + ".");
   return "nieznany ruch";
 }
-
-printMessage("Ruch komputera to: " + computerMove);
-
-let playerInput = prompt("Wybierz swój ruch! 1: kamien, 2: papier, 3: nozyce.");
-
-console.log("Gracz wpisał: " + playerInput);
-
-let playerMove = getMoveName;
-
-if (playerInput == "1") {
-  playerMove = "kamien";
-} else if (playerInput == "2") {
-  playerMove = "nozyce";
-} else if (playerInput == "3") {
-  playerMove = "papier";
-}
-
-printMessage("Twój ruch to: " + playerMove);
 
 function displayResult(computerMove, playerMove) {
   printMessage("Zagralem " + computerMove + " , a Ty " + playerMove);
@@ -56,5 +65,3 @@ function displayResult(computerMove, playerMove) {
     printMessage("Wybierz numer od 1 do 3");
   }
 }
-
-displayResult(computerMove, playerMove);
